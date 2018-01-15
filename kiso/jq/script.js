969b487451dd56861391e19dfec00f29170ec8ad
matsuno79 <matuno79@gmail.com>
2018-01-15 17:38:38 +0900

    2018.1.15-2

---
+++ kiso/jq/script.js
@@ -0,0 +1,14 @@
+
+ 
+  $(function(){
+      
+    $("h1").click(function{
+            $(this).css("color","red");
+                  
+                        
+    });
+});
+
+
+insert into item(item?id,item_name,item_team,price)values
+                 
---
+++ kiso/js/script.js
@@ -0,0 +1,68 @@
+
+
+//document.write("hello world!");
+//alert("次のページに移動しようとしています");
+//confirm("入力を確定してもよろしいでしょうか");
+
+/*
+var a1 = "tanaka";
+document.write(a1);*/
+
+/*
+var a1 = 10/2;
+document.write(a1);*/
+
+/*
+var a = "3+3"
+document.write(a);*/
+
+
+//document.write(10%3);
+//剰余（わり算の余り）が表示される。
+
+/*
+var a = 10
+document.write(a--);
+document.write(a);
+document.write(a);*/
+
+/*
+var a = 15;
+if(a>10){ 
+    document.write(a);}*/
+
+/*
+var a = 7;
+    if(a<10){
+        
+        document.write("b");
+        
+    }*/
+
+/*
+var a = 5;
+if(a < 10 && a >3 );{
+document.write("A");
+}
+*/
+
+
+/*
+var ritu = 3.14;
+var s5 = 5*5*ritu;
+var s7 = 7*7*3.14;
+var s10 = 10*10*3.14;
+
+document.write("半径５ｃｍ、７ｃｍ，１０ｃｍの面積はそれぞれ",s5,"と",s7,"と",s10);
+*/
+
+
+/*
+var ad = 500;
+var ch = 200;
+
+var a = ad*2 + ch*4;
+var b = ad*1 + ch*5;
+var c = ad*3 + ch*7;
+
+document.write("ABCそれぞれのグループの入園料は",a,"円,",b,"円,",c,"円です");*/
